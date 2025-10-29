44b66af3e30cd08148b466fdb62cf5e9d0370f05
Alex Bradley <alexandre.bradley@shopify.com>
2025-10-29 15:43:37 -0400

    use @shopify/eslint-plugin (#31)
    * use @shopify/eslint-plugin
    
    * remove rule that allows `any`

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import shopifyPlugin from '@shopify/eslint-plugin';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'test-app/**'],
+  },
+  ...shopifyPlugin.configs.typescript,
+  ...shopifyPlugin.configs.prettier,
+  {
+    rules: {
+      // This library doesn't use component boundaries
+      '@shopify/strict-component-boundaries': 'off',
+      // Allow file extensions in imports (standard for ES modules)
+      'import-x/extensions': 'off',
+      // Allow inline comments
+      'line-comment-position': 'off',
+      // Allow buildSchema (not just buildClientSchema)
+      '@shopify/typescript-prefer-build-client-schema': 'off',
+    },
+  },
+];
