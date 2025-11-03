e39039be2b794d1ea4215f43d30dd6d9584db2bd
Sagarika Dasgupta <saga.dasgupta@shopify.com>
2025-11-03 09:09:30 -0600

    adding telemetry to info command as well

--- src/methods/get-function-info.ts
+++ src/methods/get-function-info.ts
@@ -33,7 +33,11 @@ export async function getFunctionInfo(functionDir: string): Promise<FunctionInfo
       '--path', functionName
     ], {
       cwd: appRootDir,
-      stdio: ['pipe', 'pipe', 'pipe']
+      stdio: ['pipe', 'pipe', 'pipe'],
+      env: {
+        ...process.env,
+        SHOPIFY_INVOKED_BY: 'shopify-function-test-helpers'
+      }
     });
 
     let stdout = '';
